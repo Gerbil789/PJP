beea013eb3df2c180196f108e41edd3531d9644d
Gerbil789 <67593297+Gerbil789@users.noreply.github.com>
2024-04-11 19:31:38 +0200

    project

--- Laboratory7/PLC_Lab7/Program.cs
+++ Laboratory7/PLC_Lab7/Program.cs
@@ -18,14 +18,23 @@ namespace PLC_Lab7
             Console.WriteLine("Parsing: " + fileName);
             var inputFile = new StreamReader(fileName);
             AntlrInputStream input = new AntlrInputStream(inputFile);
+
+            // Create a lexer to tokenize the input
             PLC_Lab7_exprLexer lexer = new PLC_Lab7_exprLexer(input);
+
+            // Create a token stream from the lexer
             CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+            // Create a parser to parse the token stream
             PLC_Lab7_exprParser parser = new PLC_Lab7_exprParser(tokens);
 
+            // Add a listener to handle parse errors
             parser.AddErrorListener(new VerboseListener());
 
+            // Parse the input and get the parse tree
             IParseTree tree = parser.prog();
 
+            // If there are no syntax errors, proceed with tree traversal
             if (parser.NumberOfSyntaxErrors == 0)
             {
                 //Console.WriteLine(tree.ToStringTree(parser));
